ad48a7e7aeb69f9a57e8b8d8736dcfc776cf586e
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-09-09 12:32:39 +0200

    some fixes

--- export_storyboard.py
+++ export_storyboard.py
@@ -3,11 +3,15 @@ import sys
 import os, os.path
 import xml.etree.ElementTree
 
+def upperfirst(x):
+    return x[0].upper() + x[1:]
+
 def camelCase(st):
     if len(st.split(" ")) == 1:
-        return st
+        return upperfirst(st)
     output = ''.join(x for x in st.title() if x.isalpha())
-    return output[0].lower() + output[1:]
+    result = output[0].lower() + output[1:]
+    return upperfirst(result)
 
 storyboardsDirPath = sys.argv[1]
 outputFilePath = sys.argv[2]
@@ -21,7 +25,7 @@ for storyboard in [i for i in os.listdir(storyboardsDirPath) if i.endswith('.sto
         customClass = attributes.get("customClass")
         if customClass:
             connections = viewController.find('connections')
-            if connections:
+            if connections is not None:
                 if len(connections.findall('segue')) > 0:
                     result += "extension " + customClass + " {\n"
                     result += "\tenum Segue {\n"
